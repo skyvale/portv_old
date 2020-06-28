5762d4e66945058e951ed91acb1daafb9b88564f
Skylar <SJValerio@student.fullsail.edu>
2020-06-28 05:40:54 -0400

    The user can now successfully use the form to add a pokemon. Validation seems to be working. The added pokemons are successfully added to localStorage to the pokemon array.

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -1,305 +1,404 @@
 <template>
-    <div class="characters-page">
-        <h1>Characters</h1>
-        <div>
-            <div class="add-pokemon">
-                <font-awesome-icon icon="plus-circle" id="add-icon" @click="openForm" />
-                <p id="add-words" @click="openForm">Add members to your team!</p>
-            </div>
-            <div class="pokemon">
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="bulbasaur" class="single-mon" width="150px" height="150px">
-            </div>
-        </div>
-        <div class="modal">
-            <form v-show="formIsClosed" action="#">
-                <div class="form-header">
-                    <h2 id="form-title">Add a Pokemon</h2>
-                    <font-awesome-icon icon="times-circle" id="close-form-btn" @click="closeForm" />
-                </div>
-                <p>
-                    <label for="nickname">Nickname</label>
-                    <input type="text" id="nickname" name="nickname">
-                </p>
-                <p>
-                    <label for="species">Species</label>
-                    <input type="text" id="species" name="species" placeholder="ex. pikachu">
-                </p>
-                <p>
-                    <label for="nature">Nature</label>
-                    <input type="text" id="nature" name="nature" placeholder="ex. rash">
-                </p>
-                <p>
-                    <label for="trait">Trait</label>
-                    <input type="text" id="trait" name="trait" placeholder="ex. likes to fight">
-                </p>
-                <button id="submit-btn" type="submit">Submit</button>                               
-            </form>
-        </div>
-    </div>
+	<div class="characters-page">
+
+		<h1>Characters</h1>
+
+		<div>
+
+			<div class="add-pokemon">
+				<font-awesome-icon icon="plus-circle" id="add-icon" @click="openForm" />
+				<p id="add-words" @click="openForm">Add members to your team!</p>
+			</div>
+
+			<div class="pokemon">
+				<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="bulbasaur" class="single-mon" width="150px" height="150px">
+			</div>
+
+		</div>
+
+		<div class="modal">
+			<form v-show="formIsClosed" @submit.prevent="formSubmission">
+				<div class="form-header">
+					<h2 id="form-title">Add a Pokemon</h2>
+					<font-awesome-icon icon="times-circle" id="close-form-btn" @click="closeForm" />
+				</div>
+				<p>
+					<label for="nickname">Nickname</label>
+					<input type="text" id="nickname" name="nickname" v-model="formNickname" required>
+				</p>
+				<p>
+					<label for="species">Species</label>
+					<input type="text" id="species" name="species" placeholder="ex. pikachu" v-model="formSpecies" required>
+				</p>
+				<p>
+					<label for="nature">Nature</label>
+					<input type="text" id="nature" name="nature" placeholder="ex. rash" v-model="formNature">
+				</p>
+				<p>
+					<label for="trait">Trait</label>
+					<input type="text" id="trait" name="trait" placeholder="ex. likes to fight" v-model="formTrait">
+				</p>
+				<button id="submit-btn" type="submit">Submit</button>
+			</form>
+		</div>
+
+	</div>
 </template>
 
 
 <script>
 export default {
-    name: 'Characters',
-    data: () => {
-        return {
-            formIsClosed: false
-        }
-    },
-    methods: {
-        createMon() {
+	name: 'Characters',
+	data: () => {
+		return {
+			formIsClosed: false,
+			formNickname: '',
+			formSpecies: '',
+			formNature: '',
+			formTrait: ''
+		}
+	},
+	methods: {
 
-            console.log("within createMon");
+		// loads pokemon
+		loadPokemon() {
 
-            // pull out object of categories from local storage
-            let data = JSON.parse(localStorage.getItem('pokemon'));
+			// console.log("within loadPokemon");
 
-            console.log("data: ", data.id);
-            if (data != null) {
+			// pull out array of categories from local storage
+			// let data = JSON.parse(localStorage.getItem('pokemon'));
 
-                // use the already stored pokemon's id to fetch info from api
-                const apiData = {
-                    url: 'https://pokeapi.co/api/v2/',
-                    type: 'pokemon', 
-                    id: `${data.id}`,
-                }
+			// console.log('data: ', data);
+		},
 
-                // this line below is called 'deconstruction'
-                const {url, type, id} = apiData;
+		// when run, this function will open the form by setting its display to block
+		openForm() {
 
-                // construct the api url for fetching data
-                const apiUrl = `${url}${type}/${id}`;
+			// form is closed is set to false
+			this.formIsClosed = false;
 
-                // fetch the pokemon data from the api
-                fetch(apiUrl)
-                    .then( (data) => data.json())
-                    .then( (pokemon) => generateMon(pokemon) )
+			// gets form html from document
+			const form = document.querySelector("form");
 
-                
-                // using the fetched data, generate pokemon
-                const generateMon = (data) => {
-                    console.log("generatemon: ", data);
+			// sets the display so the form is visible
+			if (form.style.display == 'none') {
+				form.style.display = 'block';
+			}
 
-                }
+		},
 
-                // for (let i = 0; data.length )
+		// this function closes the form by setting its display to none
+		// form is closed is set to true
+		closeForm() {
 
-            } else if (data === null) {
+			this.formIsClosed = true;
 
-                console.log("Within null.");
+			const form = document.querySelector("form");
 
-                /*
-                // create three default pokemon to populate page
-                const defaultMonUrls = [
-                    'https://pokeapi.co/api/v2/pokemon/1',
-                    'https://pokeapi.co/api/v2/pokemon/4',
-                    'https://pokeapi.co/api/v2/pokemon/7'
-                ];
+			if (form.style.display == 'block') {
+				form.style.display = 'none';
+			}
 
-                // using the fetched data, generate pokemon
+			this.formIsClosed = !this.formIsClosed // tbh not sure what this line means, but it works *shrug*
+		},
 
-                    // load in default pokemon from api
-                    Promise.all(defaultMonUrls.map( url =>
-                        fetch(url)
-                            .then( (data) => data.json())               
-                    ))
-                    .then( (pokemon) => generateDefault(pokemon) ) 
+		// this function is called when the user submits a new pokemon using the form
+		formSubmission() {
 
-                    // select the <div> where the pokemon will be inserted
-                    const pokemonHtml = document.querySelector(".pokemon");
+			// check if data is all correct
+			/*
+			console.log({
+				Nickname: this.formNickname,
+				Species: this.formSpecies,
+				Nature: this.formNature,
+				Trait: this.formTrait
+			});
+			*/
 
-                    // generate default pokemon on the page
-                    const generateDefault = (data) => {
+			// make sure the user input is lowercase
+			const newNickname = this.formNickname;
+			const newSpecies = this.formSpecies;
+			const newNature = this.formNature;
+			const newTrait = this.formTrait;
 
-                        let htmlTemplate = ``;
-                        for (let i = 0; i < 3; i++){
-                            htmlTemplate = `
-                                <img src="${data[i].sprites.front_default}" alt="${data[i].name}" class="single-mon" width="150px" height="150px">
-                                <p>Testing...</p>
-                            `
-                            console.log("html: ", htmlTemplate);
-                            pokemonHtml.insertAdjacentHTML = ('beforeend', htmlTemplate);
+			// create apiUrl based on the specified pokemon species
+			const apiUrl = 'https://pokeapi.co/api/v2/pokemon/' + `${this.formSpecies}`;
 
-                            // set local storage
-                            localStorage.setItem('pokemon', JSON.stringify(data));
-                        }
-                    }
-                */              
-            }
-        },
-        openForm() {
-            
-            // when run, this function will open the form by setting its display to block
-            // form is closed is set to false
+			// fetch the pokemon data from the api
+			fetch(apiUrl)
+				.then( (data) => data.json())
+				.then( (pokemon) => createNewPokemon(pokemon) )
+				.catch( (error) => {
+					// API call failed, user likely didn't type in a valid pokemon species
+					console.log(error);
+					alert("Please type a valid pokemon species!");
 
-            this.formIsClosed = false;
+				})
 
-            const form = document.querySelector("form");
+			// using the fetched data, generate pokemon
+			const createNewPokemon = (data) => {
 
-            if (form.style.display == 'none') {
-              form.style.display = 'block';
-            }
+				console.log("New Pokemon: ", data);
 
-        },
-        closeForm() {
+				let newPokemon = {};
 
-            // this function closes the form by setting its display to none
-            // form is closed is set to true
+				// some pokemon have more than one type
+				if ( data.types.length === 2 ) { // this will run if the pokemon IS duel type
 
-            this.formIsClosed = true;
+					newPokemon = {
+						nickname: newNickname,
+						species: newSpecies,
+						type: data.types[0].type.name,
+						type2: data.types[1].type.name,
+						ability: data.abilities[0].ability.name,
+						nature: newNature,
+						trait: newTrait,
+						imgUrl: data.sprites.front_default
+					}
 
-            const form = document.querySelector("form");
+					console.log("Created duel type: ", newPokemon);
 
-            if (form.style.display == 'block') {
-              form.style.display = 'none';
-            }
+					// if localStorage is empty, you cant .push to null, so...
+					if (localStorage.length === 0) {
+					
+						// put the new pokemon into an empty array
+						let pokemonArray = [newPokemon];
+						console.log('pokearray: ', pokemonArray);
 
-            this.formIsClosed = !this.formIsClosed // not sure what this line means, but it works
-        }
-    },
-    mounted() {
-        this.createMon();
-    }
+						// shove the pokemon back in local storage
+						localStorage.setItem('pokemon', JSON.stringify(pokemonArray));						
+
+					} else { // if localStorage not empty
+
+						// pulls current pokemon from storage
+						let storedMons = JSON.parse(localStorage.getItem('pokemon')) || [];
+						console.log('localStorage: ', storedMons);
+
+						// appends the new pokemon to the array of current pokemon
+						storedMons.push(newPokemon);
+
+						// shove all the pokemon back in local storage
+						localStorage.setItem('pokemon', JSON.stringify(storedMons));					
+					
+					}
+
+					// clear the inputs
+					document.querySelector('form').reset();
+
+					// close the form
+					this.closeForm();
+
+					// reloads the page so it can update to current
+					// location.reload();
+
+				} else { // this will run if the pokemon is monotype
+
+					newPokemon = {
+						nickname: newNickname,
+						species: newSpecies,
+						type: data.types[0].type.name,
+						ability: data.abilities[0].ability.name,
+						nature: newNature,
+						trait: newTrait,
+						imgUrl: data.sprites.front_default
+					}
+
+					console.log("Created mono type: ", newPokemon);
+
+
+					// if localStorage is empty, you cant .push to null, so...
+					if (localStorage.length === 0) {
+					
+						// put the new pokemon into an empty array
+						let pokemonArray = [newPokemon];
+						console.log('pokearray: ', pokemonArray);
+
+						// shove the pokemon back in local storage
+						localStorage.setItem('pokemon', JSON.stringify(pokemonArray));						
+
+					} else { // if localStorage not empty
+
+						// set an empty array to hold the pokemon
+						let pokemonArray = [];
+
+						// pulls current pokemon from storage
+						let storedMons = JSON.parse(localStorage.getItem('pokemon')) || [];
+						console.log('localStorage: ', storedMons);
+
+						pokemonArray = [storedMons];
+
+						// appends the new pokemon to the array of current pokemon
+						pokemonArray.push(newPokemon);
+						console.log('pokearray: ', pokemonArray);
+
+						// shove all the pokemon back in local storage
+						localStorage.setItem('pokemon', JSON.stringify(pokemonArray));					
+					
+					}
+
+					// clear the inputs
+					document.querySelector('form').reset();
+
+					// close the form
+					this.closeForm();
+
+					// reloads the page so it can update to current
+					// location.reload();
+
+				}
+
+			}
+
+		}
+	},
+	mounted() {
+		this.loadPokemon();
+	}
 }
 </script>
 
 
-<style scoped> 
+<style scoped>
 
-    .characters-page {
-        width: 1600px;
-        margin: 0 auto;
-    }
+	.characters-page {
+		width: 1600px;
+		margin: 0 auto;
+	}
 
-    h1 {
-        color: transparent;
-    }
+	h1 {
+		color: transparent;
+	}
 
-    .pokemon {
-        display: flex;
-        flex-direction: row;
-    }
+	.pokemon {
+		display: flex;
+		flex-direction: row;
+	}
 
-    .single-mon {
-        display: inline-block;
-        width: 150px;
-        height: 150px;
-        padding: 1rem;
-        border: 1px solid grey;
-        border-radius: 50%;
-    }
+	.single-mon {
+		display: inline-block;
+		width: 150px;
+		height: 150px;
+		padding: 1rem;
+		border: 2px solid #FFE5E5;
+		border-radius: 50%;
+	}
+	.single-mon:hover {
+		cursor: pointer;
+		background-color: #FFE5E5;
+		transform: scale(1.2, 1.2);
+	}
 
-    /* Add members to your team button*/
-    .add-pokemon {
-        width: 1600px;
-        margin: 0 auto;
-        display: flex;
-        justify-content: center;
-        padding: 1rem;
-    }
-    #add-words {
-        padding-left: 10px;
-        display: inline-block;
-        color: #FA5959;
-        font-size: 1.8rem;
-    }
-    #add-words:hover {
-        cursor: pointer;
-    }
-    #add-icon {
-        display: inline-block;
-        color: #FA5959;
-        height: 30px;
-        width: 30px;
-    }
-    #add-icon:hover {
-        cursor: pointer;
-    }
+	/* Add members to your team button*/
+	.add-pokemon {
+		width: 1600px;
+		margin: 0 auto;
+		display: flex;
+		justify-content: center;
+		padding: 1rem;
+	}
+	#add-words {
+		padding-left: 10px;
+		display: inline-block;
+		color: #FA5959;
+		font-size: 1.8rem;
+	}
+	#add-words:hover {
+		cursor: pointer;
+	}
+	#add-icon {
+		display: inline-block;
+		color: #FA5959;
+		height: 30px;
+		width: 30px;
+	}
+	#add-icon:hover {
+		cursor: pointer;
+	}
 
-    /* FORM */
+	/* FORM */
 
-    .modal {
-        position: fixed;
-        z-index: 1000;
-        top: 50%;
-        left: 50%;
-    }
+	.modal {
+		position: fixed;
+		z-index: 1000;
+		top: 50%;
+		left: 50%;
+	}
 
-    form {
-        display: none;
-        transform: translate(-50%, -50%);
-        text-align: center;
-        margin: 0 auto;
-        width: 600px;
-        border: 3px solid #FA5959;
-        background-color: #fff;
-        border-radius: 5px;
-    }
+	form {
+		display: none;
+		transform: translate(-50%, -50%);
+		text-align: center;
+		margin: 0 auto;
+		width: 600px;
+		border: 3px solid #FA5959;
+		background-color: #fff;
+		border-radius: 5px;
+	}
 
-    #form-title {
-        font-size: 1.8rem;
-        color: #fff;
-        padding-top: 2rem;
-    }
+	#form-title {
+		font-size: 1.8rem;
+		color: #fff;
+		padding-top: 2rem;
+	}
 
-    .form-header {
-        text-align: center;
-        margin: 0 auto;
-        height: 100px;
-        width: 600px;
-        border-top-left-radius: 1px;
-        border-top-right-radius: 1px;
-        background-color: #FA5959;
-    }
+	.form-header {
+		text-align: center;
+		margin: 0 auto;
+		height: 100px;
+		width: 600px;
+		border-top-left-radius: 1px;
+		border-top-right-radius: 1px;
+		background-color: #FA5959;
+	}
 
-    form p {
-        text-align: left;
-        font-size: 1.4rem;
-        display: block;
-        padding: 1rem;
-        margin-bottom: 0.5rem;
-    }
+	form p {
+		text-align: left;
+		font-size: 1.4rem;
+		display: block;
+		padding: 1rem;
+		margin-left: 1rem;
+		margin-bottom: 0.5rem;
+	}
 
-    form input {
-        display: block;
-        color: #8B1717;
-        background-color: #FFE5E5;
-        border: none;
-        margin-top: 10px;
-        height: 35px;
-        width: 500px;
-    }
+	form input {
+		display: block;
+		color: #8B1717;
+		background-color: #FFE5E5;
+		border: none;
+		margin-top: 10px;
+		height: 35px;
+		width: 500px;
+	}
 
-    #submit-btn {
-        font-family: 'Prompt', sans-serif;
-        font-size: 1.6rem;
-        color: #fff;
-        background-color: #FA5959;
-        margin: 2rem 0 1rem 0;
-        width: 200px;
-        height: 50px;
-        text-align: center;
-        border: none;
-        border-radius: 5px;
-    }
-    #submit-btn:hover {
-        cursor: pointer;
-        background-color: #8B1717;
-    }
+	#submit-btn {
+		font-family: 'Prompt', sans-serif;
+		font-size: 1.6rem;
+		color: #fff;
+		background-color: #FA5959;
+		margin: 2rem 0 1rem 0;
+		width: 200px;
+		height: 50px;
+		text-align: center;
+		border: none;
+		border-radius: 5px;
+	}
+	#submit-btn:hover {
+		cursor: pointer;
+		background-color: #8B1717;
+	}
 
-    #close-form-btn {
-        display: block;
-        position: absolute;
-        right: 15px;
-        top: 15px;
-        color: #fff;
-        height: 30px;
-        width: 30px;
-    }
-    #close-form-btn:hover {
-        cursor: pointer;
-        color: #8B1717;
-    }
+	#close-form-btn {
+		display: block;
+		position: absolute;
+		right: 15px;
+		top: 15px;
+		color: #fff;
+		height: 30px;
+		width: 30px;
+	}
+	#close-form-btn:hover {
+		cursor: pointer;
+		color: #8B1717;
+	}
 
 </style>
